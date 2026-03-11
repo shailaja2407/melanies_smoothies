7991247a1cf9a965bc15f50c9b04a31603101df1
shailaja2407 <shailaja.bandella@gmail.com>
2026-03-11 20:15:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,35 +43,28 @@ ingredients_list = st.multiselect(
 if ingredients_list:
 
     ingredients_string = ""
-
     for fruit_chosen in ingredients_list:
-
+    
         ingredients_string += fruit_chosen + " "
-
-        # Get SEARCH_ON value from pandas dataframe
+    
         search_on = pd_df.loc[
             pd_df["FRUIT_NAME"] == fruit_chosen,
             "SEARCH_ON"
         ].iloc[0]
-
-        st.write("The search value for", fruit_chosen, "is", search_on, ".")
-
-
-        # Show section title
+    
+        st.write("The search value for", fruit_chosen, "is", search_on)
+    
         st.subheader(fruit_chosen + " Nutrition Information")
-
-
-        # Call SmoothieFroot API
+    
         fruityvice_response = requests.get(
-            "https://my.smoothiefroot.com/api/fruit/" + search_on
+            "https://my.smoothiefroot.com/api/fruit/" + str(search_on)
         )
-
-
-        # Display nutrition data
+    
         st.dataframe(
             data=fruityvice_response.json(),
             use_container_width=True
         )
+    
 
 
     # Insert smoothie order
