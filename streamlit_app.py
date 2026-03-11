2734177ffb91cfc7ac3eeeabf45e0ff7a1c18fda
shailaja2407 <shailaja.bandella@gmail.com>
2026-03-11 20:11:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,10 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
+import pandas as pd
 
 
-# App Title
+# App title
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write("Choose the fruits you want in your custom Smoothie!")
 
@@ -19,17 +20,21 @@ name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your Smoothie will be:", name_on_order)
 
 
-# Get fruit options from Snowflake
-my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"))
+# Get fruit options table
+my_dataframe = session.table("smoothies.public.fruit_options").select(
+    col("FRUIT_NAME"),
+    col("SEARCH_ON")
+)
 
-# Convert Snowflake dataframe to list
-fruit_list = my_dataframe.to_pandas()["FRUIT_NAME"].tolist()
+
+# Convert Snowpark dataframe → Pandas dataframe
+pd_df = my_dataframe.to_pandas()
 
 
-# Ingredient selector
+# Fruit selector
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:",
-    fruit_list,
+    pd_df["FRUIT_NAME"].tolist(),
     max_selections=5
 )
 
@@ -42,28 +47,41 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
 
         ingredients_string += fruit_chosen + " "
-        st.subheader(fruit_chosen + ' Nutrition Information')
 
-        # Call SmoothieFroot API for each selected fruit
-        smoothiefroot_response = requests.get(
-            f"https://my.smoothiefroot.com/api/fruit/" + fruit_chosen
+        # Get SEARCH_ON value from pandas dataframe
+        search_on = pd_df.loc[
+            pd_df["FRUIT_NAME"] == fruit_chosen,
+            "SEARCH_ON"
+        ].iloc[0]
+
+        st.write("The search value for", fruit_chosen, "is", search_on, ".")
+
+
+        # Show section title
+        st.subheader(fruit_chosen + " Nutrition Information")
+
+
+        # Call SmoothieFroot API
+        fruityvice_response = requests.get(
+            "https://my.smoothiefroot.com/api/fruit/" + search_on
         )
+
 
         # Display nutrition data
         st.dataframe(
-            data=smoothiefroot_response.json(),
+            data=fruityvice_response.json(),
             use_container_width=True
         )
 
 
-    # Insert order into Snowflake
+    # Insert smoothie order
     my_insert_stmt = f"""
         insert into smoothies.public.orders(ingredients, name_on_order)
         values ('{ingredients_string}', '{name_on_order}')
     """
 
 
-    # Submit order button
+    # Submit button
     time_to_insert = st.button("Submit Order")
 
     if time_to_insert:
